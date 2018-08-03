ffd6403e47843c1e083fd9f0f28c92e08783359d
ykbfb <570097211@qq.com>
2018-08-03 16:40:53 +0800

    修改执行命令

--- behaveRun .cmd
+++ behaveRun .cmd
@@ -1,19 +1,19 @@
 cd F:\DFSS_Auto_Test_BDD
 F:
-behave --tags=user_login
-behave --tags=createNewClient
-behave --tags=modify_lnk
-behave --tags=finance_book
-behave --tags=create_come_intview
-behave --tags=approve_intview
-behave --tags=create_vip_contract
-behave --tags=create_vip_contract
-behave --tags=arc_vip_contract_cmp
-behave --tags=bill_recept
-behave --tags=create_salesOrder_cmp
-behave --tags=salesOrder_oprate
-behave --tags=my_order
-behave --tags=apprv_channelresult
-behave --tags=share_client
-behave --tags=del_client
-behave --tags=cus_call
+behave --tags=user_login >F:\DFSS_Auto_Test_BDD\BDD_Run_Logs\login.txt
+behave --tags=createNewClient >F:\DFSS_Auto_Test_BDD\BDD_Run_Logs\createNewClient.txt
+behave --tags=modify_lnk >F:\DFSS_Auto_Test_BDD\BDD_Run_Logs\modify_lnk_man.txt
+behave --tags=finance_book >F:\DFSS_Auto_Test_BDD\BDD_Run_Logs\finance_book.txt
+behave --tags=create_come_intview >F:\DFSS_Auto_Test_BDD\BDD_Run_Logs\create_come_intview.txt
+behave --tags=approve_intview >F:\DFSS_Auto_Test_BDD\BDD_Run_Logs\approve_intview.txt
+behave --tags=create_vip_contract >F:\DFSS_Auto_Test_BDD\BDD_Run_Logs\create_vip_contract.txt
+behave --tags=create_vip_contract >F:\DFSS_Auto_Test_BDD\BDD_Run_Logs\create_vip_contract.txt
+behave --tags=arc_vip_contract_cmp >F:\DFSS_Auto_Test_BDD\BDD_Run_Logs\arc_vip_contract_cmp.txt
+behave --tags=bill_recept >F:\DFSS_Auto_Test_BDD\BDD_Run_Logs\bill_recept.txt
+behave --tags=create_salesOrder_cmp >F:\DFSS_Auto_Test_BDD\BDD_Run_Logs\create_salesOrder_cmp.txt
+behave --tags=salesOrder_oprate >F:\DFSS_Auto_Test_BDD\BDD_Run_Logs\salesOrder_oprate.txt
+behave --tags=my_order >F:\DFSS_Auto_Test_BDD\BDD_Run_Logs\my_order.txt
+behave --tags=apprv_channelresult >F:\DFSS_Auto_Test_BDD\BDD_Run_Logs\apprv_channelresult.txt
+behave --tags=share_client >F:\DFSS_Auto_Test_BDD\BDD_Run_Logs\share_client.txt
+behave --tags=del_client >F:\DFSS_Auto_Test_BDD\BDD_Run_Logs\del_client.txt
+behave --tags=cus_call >F:\DFSS_Auto_Test_BDD\BDD_Run_Logs\cus_call.txt
