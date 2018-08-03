58f3f639ad9fd8b43f47aa22061fe20a2abdc815
ykbfb <570097211@qq.com>
2018-08-03 17:05:57 +0800

    修改执行命令-路径

--- behaveRun .cmd
+++ behaveRun .cmd
@@ -1,19 +1,19 @@
 cd F:\DFSS_Auto_Test_BDD
 F:
-behave --tags=user_login >F:\DFSS_Auto_Test_BDD\BDD_Run_Logs\login.txt
-behave --tags=createNewClient >F:\DFSS_Auto_Test_BDD\BDD_Run_Logs\createNewClient.txt
-behave --tags=modify_lnk >F:\DFSS_Auto_Test_BDD\BDD_Run_Logs\modify_lnk_man.txt
-behave --tags=finance_book >F:\DFSS_Auto_Test_BDD\BDD_Run_Logs\finance_book.txt
-behave --tags=create_come_intview >F:\DFSS_Auto_Test_BDD\BDD_Run_Logs\create_come_intview.txt
-behave --tags=approve_intview >F:\DFSS_Auto_Test_BDD\BDD_Run_Logs\approve_intview.txt
-behave --tags=create_vip_contract >F:\DFSS_Auto_Test_BDD\BDD_Run_Logs\create_vip_contract.txt
-behave --tags=create_vip_contract >F:\DFSS_Auto_Test_BDD\BDD_Run_Logs\create_vip_contract.txt
-behave --tags=arc_vip_contract_cmp >F:\DFSS_Auto_Test_BDD\BDD_Run_Logs\arc_vip_contract_cmp.txt
-behave --tags=bill_recept >F:\DFSS_Auto_Test_BDD\BDD_Run_Logs\bill_recept.txt
+behave --tags=user_login >G:\DFSS_Auto_Test_BDD\BDD_Run_Logs\login.txt
+behave --tags=createNewClient >G:\DFSS_Auto_Test_BDD\BDD_Run_Logs\createNewClient.txt
+behave --tags=modify_lnk >G:\DFSS_Auto_Test_BDD\BDD_Run_Logs\modify_lnk_man.txt
+behave --tags=finance_book >G:\DFSS_Auto_Test_BDD\BDD_Run_Logs\finance_book.txt
+behave --tags=create_come_intview >G:\DFSS_Auto_Test_BDD\BDD_Run_Logs\create_come_intview.txt
+behave --tags=approve_intview >G:\DFSS_Auto_Test_BDD\BDD_Run_Logs\approve_intview.txt
+behave --tags=create_vip_contract >G:\DFSS_Auto_Test_BDD\BDD_Run_Logs\create_vip_contract.txt
+behave --tags=create_vip_contract >G:\DFSS_Auto_Test_BDD\BDD_Run_Logs\create_vip_contract.txt
+behave --tags=arc_vip_contract_cmp >G:\DFSS_Auto_Test_BDD\BDD_Run_Logs\arc_vip_contract_cmp.txt
+behave --tags=bill_recept >G:\DFSS_Auto_Test_BDD\BDD_Run_Logs\bill_recept.txt
 behave --tags=create_salesOrder_cmp >F:\DFSS_Auto_Test_BDD\BDD_Run_Logs\create_salesOrder_cmp.txt
-behave --tags=salesOrder_oprate >F:\DFSS_Auto_Test_BDD\BDD_Run_Logs\salesOrder_oprate.txt
-behave --tags=my_order >F:\DFSS_Auto_Test_BDD\BDD_Run_Logs\my_order.txt
-behave --tags=apprv_channelresult >F:\DFSS_Auto_Test_BDD\BDD_Run_Logs\apprv_channelresult.txt
-behave --tags=share_client >F:\DFSS_Auto_Test_BDD\BDD_Run_Logs\share_client.txt
-behave --tags=del_client >F:\DFSS_Auto_Test_BDD\BDD_Run_Logs\del_client.txt
-behave --tags=cus_call >F:\DFSS_Auto_Test_BDD\BDD_Run_Logs\cus_call.txt
+behave --tags=salesOrder_oprate >G:\DFSS_Auto_Test_BDD\BDD_Run_Logs\salesOrder_oprate.txt
+behave --tags=my_order >G:\DFSS_Auto_Test_BDD\BDD_Run_Logs\my_order.txt
+behave --tags=apprv_channelresult >G:\DFSS_Auto_Test_BDD\BDD_Run_Logs\apprv_channelresult.txt
+behave --tags=share_client >G:\DFSS_Auto_Test_BDD\BDD_Run_Logs\share_client.txt
+behave --tags=del_client >G:\DFSS_Auto_Test_BDD\BDD_Run_Logs\del_client.txt
+behave --tags=cus_call >G:\DFSS_Auto_Test_BDD\BDD_Run_Logs\cus_call.txt
